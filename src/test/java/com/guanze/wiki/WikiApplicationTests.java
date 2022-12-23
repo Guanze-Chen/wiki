c6aff27c27aec2d1b802eb83b19c31287c97c0ab
Guanze <ScuGuanze@163.com>
2022-12-23 11:22:17 +0800

    2-2 init

---
+++ src/test/java/com/guanze/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guanze.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
