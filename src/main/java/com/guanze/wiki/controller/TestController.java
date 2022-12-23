d29a7afc44ffcc818e8545ae57e3e62816b26d47
Guanze <1625205886@qq.com>
2022-12-23 15:45:37 +0800

    2-9 集成热部署

--- src/main/java/com/guanze/wiki/controller/TestController.java
+++ src/main/java/com/guanze/wiki/controller/TestController.java
@@ -1,14 +1,17 @@
 package com.guanze.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.hello:test}")
+    private String testHello;
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
 
     @PostMapping("/hello/post")
