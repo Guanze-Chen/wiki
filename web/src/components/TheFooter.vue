4741a17a431cecaeac1b1de80b4e11d19fb68eda
Guanze <1625205886@qq.com>
2022-12-24 16:43:54 +0800

    4-7 制作header footer组件

---
+++ web/src/components/TheFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    wiki created by vue3 antd
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TheFooter'
+});
+</script>
