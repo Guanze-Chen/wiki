a6fb3ec8cb9645595b814eb77a941a2022a55a40
Guanze <1625205886@qq.com>
2022-12-25 17:10:45 +0800

    5-8 增加AOP 打印接口耗时，请求参数和返回参数 去掉过滤器 拦截器

--- src/main/java/com/guanze/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/guanze/wiki/config/SpringMvcConfig.java
@@ -1,21 +1,21 @@
-package com.guanze.wiki.config;
-
-
-import com.guanze.wiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-    @Resource
-    LogInterceptor logInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**");
-    }
-
-}
+//package com.guanze.wiki.config;
+//
+//
+//import com.guanze.wiki.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**");
+//    }
+//
+//}
