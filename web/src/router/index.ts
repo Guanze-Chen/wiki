8098435102dc206c0eccd1af65614c41d22b7594
Guanze <1625205886@qq.com>
2022-12-28 15:13:00 +0800

    8-12 增加文档具体内容页面，点击首页电子书可以进入文档页面

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -38,6 +38,14 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/admin/Doc.vue')
+  },
+  {
+    path: '/docdetail',
+    name: 'DocDetail',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/DocDetail.vue')
   }
 ]
 
