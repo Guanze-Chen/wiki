73f89cf8351d013e3d05307658f98bfae5552fd2
Guanze <1625205886@qq.com>
2022-12-30 09:18:30 +0800

    9-13 增加登录拦截,未登录时跳回首页

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import store from "@/store";
+import {Tool} from "@/utils/tool";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -21,7 +23,10 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/admin/Ebook.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/admin/Ebook.vue'),
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/category',
@@ -29,7 +34,10 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/admin/Category.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/admin/Category.vue'),
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/doc',
@@ -37,7 +45,10 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/admin/Doc.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/admin/Doc.vue'),
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/docdetail',
@@ -53,7 +64,10 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/admin/User.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/admin/User.vue'),
+    meta: {
+      loginRequire: true
+    }
   }
 ]
 
@@ -61,5 +75,24 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+// 路由登录拦截
+router.beforeEach((to, from, next) => {
+  // 要不要对meta.loginRequire属性做监控拦截
+  if (to.matched.some(function (item) {
+    console.log(item, "是否需要登录校验：", item.meta.loginRequire);
+    return item.meta.loginRequire
+  })) {
+    const loginUser = store.state.user;
+    if (Tool.isEmpty(loginUser)) {
+      console.log("用户未登录！");
+      next('/');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
