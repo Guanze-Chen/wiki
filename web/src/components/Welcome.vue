6d4715998ef2ea07bc9fb4c069d0fc2c985c7540
Guanze <1625205886@qq.com>
2023-01-03 14:56:09 +0800

    11-8 echarts文件导入和示例

--- web/src/components/Welcome.vue
+++ web/src/components/Welcome.vue
@@ -88,14 +88,22 @@
           </a-card>
         </a-col>
       </a-row>
+      <br />
+      <a-row>
+        <a-col :span="24">
+          <div id="main" style="width: 100%; height: 300px;"></div>
+        </a-col>
+      </a-row>
 
     </div>
 </template>
 
-<script>
+<script lang="ts">
 import {defineComponent, ref, onMounted} from "vue";
 import axios from 'axios';
 import {UserOutlined, LikeOutlined, ArrowDownOutlined, ArrowUpOutlined} from '@ant-design/icons-vue';
+
+declare let echarts: any;
 
 export default defineComponent({
   name: "TheWelcome",
@@ -146,8 +154,33 @@ export default defineComponent({
             }
           });
     };
+
+
+    const testEcharts = (() => {
+      const myChart = echarts.init(document.getElementById('main'));
+// 绘制图表
+      myChart.setOption({
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      });
+    })
+
     onMounted(() => {
       getStat();
+      testEcharts();
     });
 
     return {
