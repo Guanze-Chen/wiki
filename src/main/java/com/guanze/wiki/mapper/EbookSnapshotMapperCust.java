70c0505eaab5693d5fc1b6251bcfa853e9496c5f
Guanze <1625205886@qq.com>
2023-01-03 12:42:01 +0800

    11-6 后端增加数值统计接口

--- src/main/java/com/guanze/wiki/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/com/guanze/wiki/mapper/EbookSnapshotMapperCust.java
@@ -1,6 +1,11 @@
 package com.guanze.wiki.mapper;
 
+import com.guanze.wiki.resp.StatisticResp;
+
+import java.util.List;
+
 public interface EbookSnapshotMapperCust {
 
     public void genSnapshot();
+    List<StatisticResp> getStatistic();
 }
--- src/main/java/com/guanze/wiki/service/EbookSnapshotService.java
+++ src/main/java/com/guanze/wiki/service/EbookSnapshotService.java
@@ -1,9 +1,11 @@
 package com.guanze.wiki.service;
 
 import com.guanze.wiki.mapper.EbookSnapshotMapperCust;
+import com.guanze.wiki.resp.StatisticResp;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class EbookSnapshotService {
@@ -13,6 +15,10 @@ public class EbookSnapshotService {
 
     public void genSnapshot() {
         ebookSnapshotMapperCust.genSnapshot();
+    }
+
+    public List<StatisticResp> getStatistic() {
+        return ebookSnapshotMapperCust.getStatistic();
     }
 }
 
